fc72982c4331115000ea6dadfee77c31b6972f57
Aljosa Sljuka <aljosa88@gmail.com>
2021-04-22 15:55:15 +0200

    join screen

---
+++ chat_ui/src/App.svelte
@@ -0,0 +1,10 @@
+<style>
+</style>
+
+<script lang="ts">
+  import Join from './join/Join.svelte'
+</script>
+
+<main>
+  <Join onJoin={name => console.log(name)} />
+</main>
---
+++ chat_ui/src/join/Join.svelte
@@ -0,0 +1,11 @@
+<style></style>
+
+<script lang="ts">
+  let name = ''
+  export let onJoin: (name: string) => void
+</script>
+
+<div>
+  <input type="text" bind:value={name}>
+  <button on:click={()  => onJoin(name)}>Join</button>
+</div>
