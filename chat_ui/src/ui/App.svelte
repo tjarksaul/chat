43cb33cd4b43f8477841b283f9b3a59a7f34d540
Geison Biazus <geisonbiazus@gmail.com>
2021-04-22 14:30:56 +0200

    Hello world => counter

---
+++ chat_ui/src/ui/App.svelte
@@ -0,0 +1,34 @@
+<style>
+  main {
+    text-align: center;
+    padding: 1em;
+    max-width: 240px;
+    margin: 0 auto;
+  }
+
+  @media (min-width: 640px) {
+    main {
+      max-width: none;
+    }
+  }
+</style>
+
+<script lang="ts">
+  import Hello from './Hello.svelte';
+
+  let count = 0;
+
+  function increment(): void {
+    count++;
+  }
+</script>
+
+<main>
+  <Hello count={count} />
+  <p>
+    Visit the <a href="https://svelte.dev/tutorial">Svelte tutorial</a> to learn
+    how to build Svelte apps.
+  </p>
+
+  <button on:click={increment}>Increment</button>
+</main>
---
+++ chat_ui/src/ui/Hello.svelte
@@ -0,0 +1,14 @@
+<style>
+    h1 {
+        color: #ff3e00;
+        text-transform: uppercase;
+        font-size: 4em;
+        font-weight: 100;
+    }
+</style>
+
+<script lang="ts">
+    export let count: number;
+</script>
+
+<h1>The count is: {count}!</h1>
