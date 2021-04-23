e2b0bc340245407b241601d9baac31953245ba8f
Aljosa Sljuka <aljosa88@gmail.com>
2021-04-23 16:05:05 +0200

    Remove unneeded value

--- chat_ui/src/chat/Room.svelte
+++ chat_ui/src/chat/Room.svelte
@@ -69,7 +69,7 @@
 
   <div class="bottom-bar">
     <form on:submit={onSubmit}>
-      <input type="text" placeholder="Your message" autofocus={true} bind:value={text}>
+      <input type="text" placeholder="Your message" autofocus bind:value={text}>
       <button type="submit">Send</button>
     </form>
   </div>
