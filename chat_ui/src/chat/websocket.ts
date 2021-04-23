e16464e8bd761a4bc34af2510943451b172f7e62
Aljosa Sljuka <aljosa88@gmail.com>
2021-04-23 13:54:34 +0200

    Format code

--- chat_ui/src/chat/websocket.ts
+++ chat_ui/src/chat/websocket.ts
@@ -1,19 +1,19 @@
-import type {Message} from "./message";
-import {addMessage} from "./chatStore";
+import type { Message } from './message'
+import { addMessage } from './chatStore'
 
-const serverUrl = 'wss://v7oia7q53f.execute-api.us-east-1.amazonaws.com/dev';
+const serverUrl = 'wss://otmyj253a4.execute-api.us-east-1.amazonaws.com/dev'
 export const socket = new WebSocket(serverUrl)
 
 socket.addEventListener('message', (event) => {
-    console.log('Message from server ', event.data)
+  console.log('Message from server ', event.data)
 
-    const {name, message} = JSON.parse(event.data)
+  const { name, message } = JSON.parse(event.data)
 
-    addMessage({name: name ?? "Error", text: message})
+  addMessage({ name: name ?? 'Error', text: message })
 })
 
-export function broadcast({name, text}: Message): void {
-    const action = {action: 'postMessage', data: {name: name, message: text}}
-    const serializedMessage = JSON.stringify(action)
-    socket.send(serializedMessage)
+export function broadcast ({ name, text }: Message): void {
+  const action = { action: 'postMessage', data: { name: name, message: text } }
+  const serializedMessage = JSON.stringify(action)
+  socket.send(serializedMessage)
 }
